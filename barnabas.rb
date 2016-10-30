0c077a4bee4a09aceb324187b8d6e3078d1304ff
Andy Nicholson <andrew@anicholson.net>
2016-10-30 13:21:36 +1100

    Fix formula-linting issues

--- barnabas.rb
+++ barnabas.rb
@@ -1,21 +1,22 @@
 class Barnabas < Formula
+  desc "Encourage your friends from your terminal"
   homepage "https://github.com/anicholson/barnabas"
   url "https://github.com/anicholson/barnabas/archive/v1.0.0-rc.1.tar.gz"
   sha256 "fd81a69abeda35f749b9855e13b07edb9d2f23695a7016b33387073226316c6c"
-  head "https://github.com/anicholson/barnabas.git", revision: "master"
+  head "https://github.com/anicholson/barnabas.git", :revision => "master", :using => :git
 
-  depends_on "crystal-lang" => :build
+  depends_on "crystal-lang" => :run
 
   def install
     system "crystal", "deps", "update"
     system "crystal", "build", "src/barnabas.cr"
-    bin.install Dir["barnabas"]
+    bin.install "barnabas"
 
-    system "mkdir", "-p", "~/.barnabas"
+    mkdir "-p", "~/.barnabas"
     system "crystal", "src/setup.cr"
   end
 
-  def test
+  test do
     system "barnabas", "-h"
   end
 end
